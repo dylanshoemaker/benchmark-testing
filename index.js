a8506142a79affcbef5864e6eccf622f334dc139
Dylan Shoemaker <me@dylanshoemaker.com>
2021-10-03 20:10:03 -0700

    test javascripts built in sort function

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 const Benchmark = require('benchmark');
-const { linearSearch, binarySearch } = require('./search');
-const { bubbleSort } = require('./sort');
+// const { linearSearch, binarySearch } = require('./search');
+// const { bubbleSort } = require('./sort');
 const { quickSort } = require('./sort');
 const numbers = [];
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 40000; i++) {
   numbers.push(Math.floor(Math.random() * 10000) + 1);
 }
 
 // grab the last number in the array as the number we want to find
-const target = numbers[numbers.length-1];
+//const target = numbers[numbers.length-1];
 
 const suite = new Benchmark.Suite;
 
 suite
-  .add('linear search', function() {
-    linearSearch(numbers, target);
-  })
-  .add('binary search', function() {
-    binarySearch(numbers, target, 0, numbers.length-1)
-  })
-  .add('bubble sort', function() {
-    const testArray = [...numbers];
+  // .add('linear search', function() {
+  //   linearSearch(numbers, target);
+  // })
+  // .add('binary search', function() {
+  //   binarySearch(numbers, target, 0, numbers.length-1)
+  // })
+  // .add('bubble sort', function() {
+  //   const testArray = [...numbers];
 
-    bubbleSort(testArray);
-  })
+  //   bubbleSort(testArray);
+  // })
   .add('quick sort', function() {
     const testArray = [...numbers];
 
     quickSort(testArray);
+  })
+  .add('js sort', function() {
+    const testArray = [...numbers];
+
+    // benchmark the built-in sort method
+    testArray.sort((a, b) => {
+      return a - b;
+    });
   })
   .on('complete', function() {
     this.forEach(result => console.log(`${result.name} averaged ${result.stats.mean*1000} milliseconds.`));
